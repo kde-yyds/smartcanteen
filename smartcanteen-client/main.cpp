24d2889a6806f351b5cdea9b2bd5196e9967241b
HeCheng Yu <kde-yyds@qq.com>
2024-03-10 22:38:33 +0800

    implement experimental config functions

--- smartcanteen-client/main.cpp
+++ smartcanteen-client/main.cpp
@@ -6,17 +6,6 @@
 
 int main(int argc, char *argv[])
 {
-
-
-    writeConfig_String("str.cfg", "amagi");
-    writeConfig_Int("int.cfg", 114514);
-
-    std::string a1; int b1;
-    readConfig_String("str.cfg", a1);
-    readConfig_Int("int.cfg", b1);
-    std::cout<<"a1="<<a1<<"b1="<<b1;
-
-
     QApplication a(argc, argv);
     MainWindow w;
     w.setFixedSize(1280,720);// TODO: make window resizeable
